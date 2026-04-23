b97b51863ad629a4c83789970b6d3ecd61764cd2
Emily Miller <ekressmiller@gmail.com>
2026-04-23 10:53:50 -0400

    adjust judgment page number offset

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.MLHDivorceAndCustody',
       version='1.0.4',
       description=('Michigan divorce and child support tools'),
-      long_description='# docassemble.MLHDivorceAndCustody\r\n\r\nMichigan divorce and custody tools\r\n\r\n## Authors:\r\nMichigan Legal Help\r\nLemma Legal\r\n\r\n## Changelog:\r\n* 4/21/26  1.0.4 changes to wording and order of questions;\r\n* 4/10/26  1.0.3 fix small font on form; fix & standardize Ottawa case initiation form reference; fix child support calculator link; update title of tool\r\n* 4/6/26   1.0.2 patch Ottawa County court selection issue; fix minor code typos\r\n* 4/1/26   1.0.1 fix issue with children not displaying in complaint #7\r\n* 3/31/26  1.0.0 initial launch of divorce complaint and judgment of divorce tools',
+      long_description='# docassemble.MLHDivorceAndCustody\r\n\r\nMichigan divorce and custody tools\r\n\r\n## Authors:\r\nMichigan Legal Help\r\nLemma Legal\r\n\r\n## Changelog:\r\n* 4/21/26  1.1.0 multiple changes to wording and order of questions\r\n* 4/10/26  1.0.3 fix small font on form; fix & standardize Ottawa case initiation form reference; fix child support calculator link; update title of tool\r\n* 4/6/26   1.0.2 patch Ottawa County court selection issue; fix minor code typos\r\n* 4/1/26   1.0.1 fix issue with children not displaying in complaint #7\r\n* 3/31/26  1.0.0 initial launch of divorce complaint and judgment of divorce tools',
       long_description_content_type='text/markdown',
       author='Michigan Legal Help',
       author_email='ekressmiller@lsscm.org',
